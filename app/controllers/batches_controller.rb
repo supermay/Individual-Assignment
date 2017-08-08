edad9879e8036ed959b317568d78c357578da27b
supermay <wu_jiamei@outlook.com>
2017-08-08 09:00:48 +0200

    Create Batch and related view

--- app/controllers/batches_controller.rb
+++ app/controllers/batches_controller.rb
@@ -3,6 +3,18 @@ class BatchesController < ApplicationController
 
   def show
     @batches = current_user.batches
+    @batch = Batch.new
+  end
+
+  def create
+    @batch = current_user.batches.build(batch_params)
+    @batch.save
+    redirect_to root_path, notice: "New batch created!"
+  end
+
+  private
+  def batch_params
+    params.require(:batch).permit(:number,:start,:end)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root "batches#show"
   devise_for :users
+  resources :batches
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
