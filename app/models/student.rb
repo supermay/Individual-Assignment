a7c7213d7633439d21035e8bc63a9f4d15d5b5dd
supermay <wu_jiamei@outlook.com>
2017-08-09 17:24:21 +0200

    Cascade Delete and some small changes

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,6 +1,6 @@
 class Student < ApplicationRecord
   belongs_to :batch
-  has_many :evaluations
+  has_many :evaluations, dependent: :destroy
   validates :picture, presence: true, uniqueness: true
   validates :first_name, :last_name,
             presence: true
