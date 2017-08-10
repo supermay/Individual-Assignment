4993190ca3d8b0eefdf1b8ec406a23976ca5aece
supermay <wu_jiamei@outlook.com>
2017-08-10 10:07:04 +0200

    Small changes

--- spec/feature/create_batches_spec.rb
+++ spec/feature/create_batches_spec.rb
@@ -2,11 +2,11 @@ require 'rails_helper'
 
 feature 'Create batches', js: true do
   scenario 'create a new batch' do
-    user = FactoryGirl.create(:user, email: "test@test.cn", password: "password")
+    user = FactoryGirl.create(:user)
     login_as(user, :scope => :user)
     # Point your browser towards the todo path
     visit root_path
-    sleep 100
+    sleep 10
     # # Enter description in the text field
     # fill_in 'todo_title', with: 'Be Batman'
     #
