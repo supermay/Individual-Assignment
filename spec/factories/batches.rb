58770c7598a3e9926e5e35aaae79f5d8d31bc8dd
supermay <wu_jiamei@outlook.com>
2017-08-10 17:35:11 +0200

    Testing batch index page

--- spec/factories/batches.rb
+++ spec/factories/batches.rb
@@ -1,8 +1,8 @@
 FactoryGirl.define do
   factory :batch do
     number    '100'
-    start_at  "08/01/2017"
-    end_at    "08/31/2017"
+    start_at  { Faker::Date.backward(14) }
+    end_at    { Faker::Date.forward(100) }
     user      {build(:user)}
   end
 end
--- spec/feature/batch_index_spec.rb
+++ spec/feature/batch_index_spec.rb
@@ -4,14 +4,40 @@ feature 'Batch Index Page', js: true do
   scenario 'create a new batch' do
     user = FactoryGirl.create(:user)
     login_as(user, :scope => :user)
-    # Point your browser towards the todo path
+    batch = FactoryGirl.create(:batch,user: user)
+    batch.save!
+
     visit root_path
     sleep 2
-    # # Enter description in the text field
-    fill_in 'Number', with: '1'
+    fill_in 'batch_number' , with: '23'
+    fill_in "batch_start_at" , with: '08/01/2017'
+    fill_in "batch_end_at" , with: '08/31/2017'
     sleep 2
+    click_on 'Save'
+    sleep 2
+    expect(page).to have_content('Batch # 23','2017-08-01 to 2017-08-31','Not added any student yet')
+    # put it separate or together?
+    sleep 2
+    fill_in 'number', with: '23'
+    click_on 'Search'
+    sleep 2
+    expect(page).to have_content('Batch # 23')
+    expect(page).not_to have_content('Batch # 100')
 
-    # # Expect the new task to be displayed in the list of tasks
-    # expect(page).to have_content('Be Batman')
   end
+
+  # scenario 'search a batch' do
+  #   user = FactoryGirl.create(:user)
+  #   login_as(user, :scope => :user)
+  #   batch1 = FactoryGirl.create(:batch)
+  #   batch1.user = user
+  #   batch1.save!
+  #
+  #   batch2 = FactoryGirl.create(:batch,number:"23")
+  #   batch2.user = user
+  #   batch2.save!
+  #
+  #   visit root_path
+  # end
+
 end
