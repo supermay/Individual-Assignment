30f9f6c5e1fbc87a9b073054eae70a57dc75d3d8
supermay <wu_jiamei@outlook.com>
2017-08-10 21:36:24 +0200

    Testing done for batch index page

--- spec/feature/batch_index_spec.rb
+++ spec/feature/batch_index_spec.rb
@@ -1,47 +1,46 @@
 require 'rails_helper'
 
+sleep_seconds = 2
+
 feature 'Batch Index Page', js: true do
   scenario 'create a batch' do
     user = FactoryGirl.create(:user)
     login_as(user, :scope => :user)
-    batch = FactoryGirl.create(:batch,user: user)
-    batch.save!
-
     visit root_path
-    sleep 2
-    fill_in 'batch_number' , with: '23'
-    fill_in "batch_start_at" , with: '08/01/2017'
-    fill_in "batch_end_at" , with: '08/31/2017'
-    sleep 2
+    sleep sleep_seconds
+    fill_in 'batch_number' , with: '3'
+    sleep sleep_seconds
+    fill_in "batch_start_at" , with: '10/01/2017'
+    sleep sleep_seconds
+    fill_in "batch_end_at" , with: '10/31/2017'
+    sleep sleep_seconds
     click_on 'Save'
-    sleep 2
-    expect(page).to have_content('Batch # 23','2017-08-01 to 2017-08-31','Not added any student yet')
-    # put it separate or together?
-    sleep 2
-    fill_in 'number', with: '23'
-    click_on 'Search'
-    sleep 2
-    expect(page).to have_content('Batch # 23')
-    expect(page).not_to have_content('Batch # 100')
-    sleep 2
-
-    click_on 'Batch # 23'
-    sleep 2
-
+    sleep sleep_seconds
+    expect(page).to have_content('Batch # 3','2017-10-01 to 2017-10-31','Not added any student yet')
   end
 
-  # scenario 'search a batch' do
-  #   user = FactoryGirl.create(:user)
-  #   login_as(user, :scope => :user)
-  #   batch1 = FactoryGirl.create(:batch)
-  #   batch1.user = user
-  #   batch1.save!
-  #
-  #   batch2 = FactoryGirl.create(:batch,number:"23")
-  #   batch2.user = user
-  #   batch2.save!
-  #
-  #   visit root_path
-  # end
+  scenario 'search a batch' do
+    user = FactoryGirl.create(:user)
+    login_as(user, :scope => :user)
+    visit root_path
+    fill_in 'number', with: '2'
+    sleep sleep_seconds
+    click_on 'Search'
+    sleep sleep_seconds
+    expect(page).to have_content('Batch # 2')
+    expect(page).not_to have_content('Batch # 1')
+  end
+
+  scenario 'Click batch and redirect to the batch show page' do
+    user = FactoryGirl.create(:user)
+    login_as(user, :scope => :user)
+    visit root_path
+    sleep sleep_seconds
+    click_on 'Batch # 1'
+    sleep sleep_seconds
+    expect(page.current_path).to eq batch_path(1)
+    expect(page.current_path).not_to eq batch_path(2)
+    sleep sleep_seconds
+  end
 
 end
